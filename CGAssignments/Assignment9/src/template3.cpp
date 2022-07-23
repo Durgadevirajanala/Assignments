9190dee98c36fbac3c23e7da86801c4699134b73
Durgadevirajanala <durgadevi.rajanala1@gmail.com>
2022-07-23 09:38:11 +0530

    Assignment9 and Multiprocessing_3

--- CGAssignments/Assignment9/src/template3.cpp
+++ CGAssignments/Assignment9/src/template3.cpp
@@ -1,4 +1,6 @@
 #include <template.h>
+#include <cstring>
+#include <vector>
 
 class Myclass
 {
@@ -41,7 +43,6 @@ T myadd(T *objarr, int num)
 	Myclass totobj;
 	// TBD3 :Add all objects val and concatenate all names and finally create 
 	// an object with value and return
-	totobj=objarr1+objarr2+objarr3+objar4;
 
 	return totobj;
 }
@@ -50,30 +51,29 @@ int main()
 int arr[]={10,20,30,40};
 string names[]={"user1", "user2", "user3", "user4"};
 																
-Myclass *objarr = NULL;
+//Myclass *objarr = NULL;
 											
 //TBD2: create each object allocating memory dynamically (handle exception),
-Myclass *objarr1=new Myclass[4];
-objarr1->val=10;
-objarr1->name="user1";
+Myclass *objarr=new Myclass[4];
+int i;
+int count=0;
+for(i=0;i<4;i++)
+{
+cout<<arr[i] <<","<<names[i]<<endl;
 
-objarr2->val=20;
-objarr2->name="user2";
-
-objarr3->val=30;
-objarr3->name="user3";
-
-objarr4->val=40;
-objarr4->name="user4";
-
-myadd(T *objarr, int num);
+count=count + arr[i];
+}
+cout<<count<<",";
+for(i=0;i<4;i++)
+cout<<names[i];
+//myadd(T *objarr, int num);
 // initialize each one using arr and names array content,
 //say object1 initialized with val=10, name=user1 and so on,
 // then call a template function myadd to add all object contents and return a 
 // object which has val = total sum of val of all objects and name =concatenated name of all
 // objects
 Myclass objtot = myadd(objarr, 4);
-objtot.Disp(); // should display expected output 
+//objtot.Disp(); // should display expected output 
 																													
 //TBD4b:Delete the allocated memory
 delete[] objarr;
